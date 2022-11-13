d63cff85d1e456b656f9d037d62b8b653df1faa0
EPourmo <94918200+EPourmo@users.noreply.github.com>
2022-11-13 20:28:56 +0100

    change tailwind prop

--- modal.js
+++ modal.js
@@ -1,20 +1,19 @@
 import React from "react";
+import closeIcon from "../assets/closeIcon.svg";
 
-const Modal = ({ setOpenModal }) => {
+const Modal = ({ setOpenModal, message }) => {
   return (
-    <div className="bg-black opacity-60 fixed inset-0 z-50">
+    <div className="bg-black bg-opacity-60 fixed inset-0 z-50">
       <div className="flex h-screen justify-center items-center">
-        <div className="flex-col justify-center bg-white py-5 px-10 border-4 border-black rounded-xl relative">
-          <div className="flex text-lg text-zinc-600 mb-10">
-            Employee Created!
-          </div>
+        <div className="flex-col justify-center bg-white py-5 px-10 border-4 border-primary rounded-xl relative">
+          <div className="flex text-base text-zinc-600">{message}</div>
           <button
-            className="absolute right-0 top-0"
+            className="absolute -right-2 -top-1.5 z-100"
             onClick={() => {
               setOpenModal(false);
             }}
           >
-            X
+            <img src={closeIcon} alt="close button" />
           </button>
         </div>
       </div>
