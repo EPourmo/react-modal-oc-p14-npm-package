3c367492e0f036a05e4ca99feb4c79d25d62c634
EPourmo <94918200+EPourmo@users.noreply.github.com>
2022-11-23 10:49:34 +0100

    add comments and modify ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# react-modal-oc-p14-npm-package
+# react-modal-oc-p14-npm-package | OpenClassrooms Project 14
+
+This repo contains a modal React component.
+This react component receives two props :
+
+- setOpenModal : function to be passed in order to control modal opening (boolean),
+- message: string message displayed by the modal
--- modal.jsx
+++ modal.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import closeIcon from "./assets/closeIcon.svg";
 import "./style/modal.css";
 
+/**
+ * Modal react component
+ * @param {String} message message displayed by the modal
+ * @param {Function} setOpenModal function to be passed in order to control modal opening (boolean)
+ * @returns react component
+ */
 const Modal = ({ setOpenModal, message }) => {
   return (
     <div className="container">
